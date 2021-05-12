498ef5ebb579ca924c485b5cda5d86bcb2ac1d0c
marstra89 <62241060+marstra89@users.noreply.github.com>
2021-05-12 10:24:56 +0200

    Update Hydroponics.c

--- Hydroponics.c
+++ Hydroponics.c
@@ -223,7 +223,7 @@ int main(int argc, char *argv[]) {
 		    int raw_adc = data[0];
 
 		    //omvandlar enligt vår ph-sensors kalibrering
-		    ph= (19.63-((4.0/57.0)*raw_adc));
+		    ph= (19.63-((1.0/15.0)*raw_adc));
 
 		    // Output data to screen
 		    printf("Ph-värde: %.2f \n", ph);
